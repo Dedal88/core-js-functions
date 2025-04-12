f90aab668c4bdbf7567fc9692006f50b766dabc1
Dedal88 <audit37msv@yandex.ru>
2025-04-12 18:40:18 +0300

    feat: solve part of the learning tasks

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -152,8 +152,24 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    let count = attempts;
+    let res;
+
+    while (count > 0) {
+      try {
+        res = func();
+        return res;
+      } catch (error) {
+        count -= 1;
+        if (count === 0) {
+          throw error;
+        }
+      }
+    }
+    return attempts;
+  };
 }
 
 /**
